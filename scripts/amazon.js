bb8121f5c9759b63c99a9a680f4a5ec7c9319eb5
jaimeoakes <jaimetro2012@gmail.com>
2025-11-03 08:20:06 -0500

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -56,8 +56,7 @@ products.forEach((product) => {
   `;
 });
 
-document.querySelector('.js-products-grid')
-  .innerHTML = productsHTML;
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
@@ -81,6 +80,12 @@ document.querySelectorAll('.js-add-to-cart')
           });
         }
 
-      console.log(cart);
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     });
   });
