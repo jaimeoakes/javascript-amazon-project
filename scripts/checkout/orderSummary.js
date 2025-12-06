0131469e22a2e08b5c8782e634bd50870559462c
jaimeoakes <jaimetro2012@gmail.com>
2025-12-06 16:50:11 -0500

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,14 +4,7 @@ import { formatCurrency } from "../utils/money.js";
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
-
-hello();
-
-const today = dayjs();
-
-const deliveryDate = today.add(7, 'days');
-
-console.log(deliveryDate.format('dddd, MMMM D'));
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
@@ -155,6 +148,8 @@ export function renderOrderSummary() {
         );
 
         container.remove();
+
+        renderPaymentSummary();
       });
     });
 
@@ -235,6 +230,7 @@ export function renderOrderSummary() {
         const {productId, deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
       });
     });
 }
