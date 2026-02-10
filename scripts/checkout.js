6e52976754278c78d27e5440688f090030ac4838
jaimeoakes <jaimetro2012@gmail.com>
2026-02-10 11:35:29 -0600

    Use fetch and Promise.all to load cart and products in checkout

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,10 +2,14 @@ import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProductsFetch } from "../data/products.js";
+import { loadCartFetch } from "../data/cart.js";
 
 async function loadPage() {
   try {
-    await loadProductsFetch();
+    await Promise.all([
+      loadProductsFetch(),
+      loadCartFetch()
+    ]);
   } catch (error) {
     console.log("unexpected error. Please try again later.");
   }
